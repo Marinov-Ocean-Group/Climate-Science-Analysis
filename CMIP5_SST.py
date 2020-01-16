439fc47801a235ac7dc598261b22c7e1264b641f
Behzad Asadieh <32144660+basadieh@users.noreply.github.com>
2020-01-16 12:07:28 -0500

    Update CMIP5_SST.py

--- CMIP5_SST.py
+++ CMIP5_SST.py
@@ -4,7 +4,6 @@
 ####     Behzad Asadieh, Ph.D.      ####
 ####  University of Pennsylvania    ####
 ####    basadieh@sas.upenn.edu      ####
-####     github.com/behzadasd       ####
 ########################################
 
 from Behzadlib import func_latlon_regrid, func_regrid, func_oceanlandmask, func_gridcell_area
